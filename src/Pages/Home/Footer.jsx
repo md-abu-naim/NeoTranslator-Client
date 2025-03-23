128672afb87980d7e7da195d252ce9628a016a9b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-23 20:20:23 +0600

    Navber structure

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        
+        <div className="py-6 bg-base-300 shadow-sm">
+            <div className=" flex items-center justify-center">
+                <h1 className=" text-center p-2 text-2xl font-bold">NeoTranslator</h1>
+            </div>
+            <footer className="footer footer-center">
+                <aside>
+                    <p><span className='font-medium'>NeoTranslator</span> can make mistakes. Check important info.</p>
+                </aside>
+            </footer>
+        </div>
     );
 };
 
