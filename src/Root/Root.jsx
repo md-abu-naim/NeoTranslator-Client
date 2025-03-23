3e53b05d0f6ba16391784fdad3006ecfc9b40713
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-23 20:19:53 +0600

    footer design

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -6,7 +6,9 @@ const Root = () => {
     return (
         <div>
             <Navber />
-            <Outlet />
+            <div className='min-h-[calc(100vh-180px)]'>
+                <Outlet />
+            </div>
             <Footer />
         </div>
     );
