82d80a1d5f13dccb7ca5973d3ca6bccbf2b6d5e0
abunaim34 <farhanadnanfarabi@gmail.com>
2025-05-22 20:20:08 +0600

    logo is not showing

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
     return (
         <div className="flex flex-col md:flex-row justify-between">
             <div className="bg-blue-300 md:w-[50%] flex flex-col  items-center">
-                <img className="h-[450px]" src="/src/assets/NeoTranslatorLogo.webp" alt="" />
+                <img className="h-[450px]" src="./assets/NeoTranslatorLogo.webp" alt="" />
                 <div className="text-center md:px-12 ">
                     <h3 className="text-2xl font-bold">Instant Translation, Anywhere.</h3>
                     <p>
